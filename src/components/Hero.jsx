b4e49352b55c1277e6d03795ec1b723fa57224c2
Shawn Ruby <103871490+Snyrby@users.noreply.github.com>
2023-06-06 16:27:10 -0700

    Worked on get started bubble of the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import styles from "../style";
 import { discount, robot } from "../assets";
+import GetStarted from "./GetStarted";
 
 const Hero = () => {
   return (
@@ -24,6 +25,9 @@ const Hero = () => {
             The Next <br className="sm:block hidden" />{" "}
             <span className="text-gradient">Generation</span>{" "} Payment Method.
           </h1>
+          <div className="ss:flex hidden md:mr-4 mr-0">
+            <GetStarted />
+          </div>
         </div>
       </div>
     </section>
